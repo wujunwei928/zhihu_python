7f839071f751d3b079045311189ea9f0a97e8534
wujunwei <wujunwei@letv.com>
2016-03-17 17:36:21 +0800

    优化爬取用户信息方法, 并将爬取到的用户信息保存到mysql数据库

--- test.py
+++ test.py
@@ -26,6 +26,10 @@ from zhihu import Answer
 from zhihu import User
 from zhihu import Collection
 
+import sys,pymysql,time
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 def question_test(url):
     question = Question(url)
@@ -98,80 +102,85 @@ def answer_test(answer_url):
     print upvote  # 输出：9320
     print visit_times  # 输出: 改答案所属问题被浏览次数
 
+def prepare_insert_sql(table_name, data):
+    sql = 'insert into '+table_name
+    key_sql = " (" + ",".join( data.keys() ) + ") "
+    val_sql = " ('" + "','".join( str(i) for i in data.values() ) + "') "
+    sql = sql + key_sql + 'values' + val_sql
+    return sql
+
 
 def user_test(user_url):
+    database_name = 'wjw_zhihu'
+    table_name = 'user_info'
+
+    # 设置数据库连接
+    conn=pymysql.connect(host='localhost',user='root',passwd='',port=3306)
+    cur=conn.cursor()
+    # 选择数据库
+    conn.select_db(database_name)
+    # 设置编码, 否则插入数据库乱码
+    cur.execute('set names utf8')
+
+    # 获取当前用户信息
     user = User(user_url)
-    # 获取用户ID
-    user_id = user.get_user_id()
-    # 获取用户性别
-    user_gender = user.get_gender()
-    # 获取该用户的关注者人数
-    followers_num = user.get_followers_num()
-    # 获取该用户关注的人数
-    followees_num = user.get_followees_num()
-    # 获取该用户提问的个数
-    asks_num = user.get_asks_num()
-    # 获取该用户回答的个数
-    answers_num = user.get_answers_num()
-    # 获取该用户收藏夹个数
-    collections_num = user.get_collections_num()
-    # 获取该用户获得的赞同数
-    agree_num = user.get_agree_num()
-    # 获取该用户获得的感谢数
-    thanks_num = user.get_thanks_num()
+    user_info = user.get_user_info()
+
+    # 将用户数据插入数据库
+    insert_sql = prepare_insert_sql(table_name, user_info)
+    # res=cur.execute(insert_sql)
+    # print(insert_sql)
+    # sys.exit()
+    # conn.commit()
+    # print(user_info['user_unique'] + '  ------  ' + str(res))
+
 
     # 获取该用户关注的人
     followees = user.get_followees()
     # 获取关注该用户的人
     followers = user.get_followers()
-    # 获取该用户提的问题
-    asks = user.get_asks()
-    # 获取该用户回答的问题的答案
-    answers = user.get_answers()
-    # 获取该用户的收藏夹
-    collections = user.get_collections()
 
-    print user_id  # 黄继新
-    print user_gender #male
-    print followers_num  # 614840
-    print followees_num  # 8408
-    print asks_num  # 1323
-    print answers_num  # 786
-    print collections_num  # 44
-    print agree_num  # 46387
-    print thanks_num  # 11477
-    print user.get_data_id()
-    import sys;
-    sys.exit();
+    # 统计该用户关注的人
+    # i = 0
+    # for followee in followees:
+    #     print followee.user_url
+    #     print followee.get_user_id()
+    #     i = i + 1
+    #     if i == 41:
+    #         break
 
-    print followees
-    # <generator object get_followee at 0x7ffcac3af050>
-    # 代表所有该用户关注的人的生成器对象
-    i = 0
-    for followee in followees:
-        print followee.user_url
-        print followee.get_user_id()
-        i = i + 1
-        if i == 41:
-            break
 
-    print followers
+    # print followers
     # <generator object get_follower at 0x7ffcac3af0f0>
     # 代表所有关注该用户的人的生成器对象
     i = 0
     for follower in followers:
-        print follower.get_user_id()
         i = i + 1
-        if i == 41:
-            break
+        if i <= 41:
+            continue
+        if i % 10 == 0:
+            time.sleep(0.3)
+        try:
+            follower_info = follower.get_user_info()
+            follower_insert_sql = prepare_insert_sql(table_name, follower_info)
+            res=cur.execute(follower_insert_sql)
+            conn.commit()
+            print(follower_info['user_unique'] + '  ------  ' + str(res))
+        except Exception as e:
+            # 打印日志, 记录异常信息
+            exceptMsg = str(e)
+            print(exceptMsg)
+        
+        
+        
 
-    print asks
+    # print asks
     # <generator object get_ask at 0x7ffcab9db780>
     # 代表该用户提的所有问题的生成器对象
-    print answers
+    # print answers
     # <generator object get_answer at 0x7ffcab9db7d0>
     # 代表该用户回答的所有问题的答案的生成器对象
-    print collections
+    # print collections
     # <generator object get_collection at 0x7ffcab9db820>
     # 代表该用户收藏夹的生成器对象
 
@@ -233,12 +242,15 @@ def main():
     # question_test(url)
     # answer_url = "http://www.zhihu.com/question/24269892/answer/29960616"
     # answer_test(answer_url)
-    user_url = "https://www.zhihu.com/people/kaifulee"
+    user_url = "https://www.zhihu.com/people/zhang-jia-wei"
+    # user_url = "https://www.zhihu.com/people/wujunwei928"
     user_test(user_url)
     # collection_url = "http://www.zhihu.com/collection/36750683"
     # collection_test(collection_url)
     # test()
 
+# 知乎话题广场: https://www.zhihu.com/topics
+# 知乎发现更多: https://www.zhihu.com/explore/recommendations
 
 if __name__ == '__main__':
     main()
